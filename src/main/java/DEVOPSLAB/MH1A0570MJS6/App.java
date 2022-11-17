9ef421bf58c95ebd0159123ad4abe256c3506270
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-11-17 10:18:18 +0530

    FIRST PROJECT FROM ECLIPSE TO SONARCUBE

---
+++ src/main/java/DEVOPSLAB/MH1A0570MJS6/App.java
@@ -0,0 +1,13 @@
+package DEVOPSLAB.MH1A0570MJS6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
